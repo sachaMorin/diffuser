a851a57974aff71a34110dee7f914e6efffae6a8
sachaMorin <morin.sacha@gmail.com>
2022-11-14 18:56:31 -0500

    Move horizon out of Manifold Planner

--- diffuser/environments/utils.py
+++ diffuser/environments/utils.py
@@ -84,12 +84,11 @@ def triu_plot(x, coords, s=20, tilt=30, rotation=-80, edgecolor='k'):
 
 
 class ManifoldPlanner:
-    def __init__(self, env, n_samples=5000, horizon=12, random_seed=42):
+    def __init__(self, env, n_samples=5000, random_seed=42):
         self.samples = env.sample(n_samples)
         self.n = self.samples.shape[0]
         self.graph = kneighbors_graph(self.samples, n_neighbors=10, mode='distance')
         self.dist, self.predecessors = shortest_path(self.graph, return_predecessors=True, directed=False)
-        self.horizon = horizon
         self.rng = np.random.RandomState(random_seed)
 
     def nearest_neighor(self, point):
@@ -120,15 +119,9 @@ class ManifoldPlanner:
 
         traj = self.samples[path]
 
-        # Lower the resolution down to horizon
-        if traj.shape[0] > self.horizon:
-            step = traj.shape[0] // self.horizon
-            traj = traj[::step]
-
-        # Use start and goal as first and last coords respectively
+        # Use start as first coord
         if start_coords is not None:
             traj[0] = start_coords
             traj[-1] = goal_coords
-
 
         return traj
